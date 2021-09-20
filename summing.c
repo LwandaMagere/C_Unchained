485f18413b393459a6a11af389302faa43c20159
Steven Dado <dadolenssen@gmail.com>
2021-09-20 08:54:38 +0300

    summing

---
+++ summing.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    long num;
+    long sum = 0L; // intialiaze sum to zero
+    int status;
+
+    printf("Please enter an integer to be summed ");
+    printf("(q to quit): ");
+    status = scanf("%ld", &num);
+    while (status == 1) 
+    {
+        sum = sum + num;
+        printf("Please enter next integer (q to quit): ");
+        status = scanf("%ld", &num);
+    }
+    printf("Those integer sum to %ld.\n", sum);
+
+    return 0;
+}
