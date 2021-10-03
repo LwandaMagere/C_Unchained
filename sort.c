1b40ca6855a3a6fb41457a97d4a6b803c0309621
LwandaMagere <dadolenssen@gmail.com>
2021-10-03 22:04:50 +0300

    jaribu tena

--- sort.c
+++ sort.c
@@ -10,30 +10,27 @@ char * s_gets(char * st, int n);
 int main(void)
 {
     char input[LIM][SIZE];
-char *ptstr[LIM];
-int ct = 0;
-int k;
+    char *ptstr[LIM];
+    int ct = 0;
+    int k;
 
-printf("Input up to %d lines, and I will sort them.\n",LIM);
-printf("To stop, press the Enter key at a line's start.\n");
-while (ct < LIM && s_gets(input[ct], SIZE) != NULL
-&& input[ct][0] != '\0')
+    printf("Input up to %d lines, and I will sort them.\n",LIM);
+    printf("To stop, press the Enter key at a line's start.\n");
+    while (ct < LIM && s_gets(input[ct], SIZE) != NULL && input[ct][0] != '\0')
 
-{
-ptstr[ct] = input[ct]; /* set ptrs to strings
-*/
-ct++;
-}
-stsrt(ptstr, ct);
-/* string sorter
-*/
-puts("\nHere's the sorted list:\n");
-for (k = 0; k < ct; k++)
-puts(ptstr[k]) ;
-/* sorted pointers
-*/
+    ptstr[ct] = input[ct]; /* set ptrs to strings */
+    ct++;
+    stsrt(ptstr, ct);
+    /* string sorter*/
+    puts("\nHere's the sorted list:\n");
+    for (k = 0; k < ct; k++)
+    puts(ptstr[k]) ;
+    /* sorted pointers*/
     return 0;
+
 }
+
+
 
 void ststr(char *strings[], int num)
 {
